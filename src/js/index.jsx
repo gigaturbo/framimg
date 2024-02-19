c726f645d8d92ac8f5ab146cae06464d02a03f94
gigaturbo <lac.baptiste@pm.me>
2024-02-19 15:45:16 +0100

    add pixi settings to index.jsx

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,20 +1,21 @@
-import { NoSsr } from "@mui/base/NoSsr";
+// import { NoSsr } from "@mui/base/NoSsr";
 import CssBaseline from "@mui/material/CssBaseline";
 import { green, indigo } from "@mui/material/colors";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
+import { settings } from "pixi.js";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-// settings.RESOLUTION = window.devicePixelRatio;
-// settings.RENDER_OPTIONS.antialias = false; //SETTINGS.render.antialias;
-// settings.RENDER_OPTIONS.autoDensity = true; //SETTINGS.render.autoDensity;
-// settings.ROUND_PIXELS = true; //SETTINGS.render.roundPixels;
+settings.RESOLUTION = window.devicePixelRatio;
+settings.RENDER_OPTIONS.antialias = false; //SETTINGS.render.antialias;
+settings.RENDER_OPTIONS.autoDensity = true; //SETTINGS.render.autoDensity;
+settings.ROUND_PIXELS = true; //SETTINGS.render.roundPixels;
 
 const theme = createTheme({
   palette: {
